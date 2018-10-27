32f3accb98d684edc0cb4ab5567eaaa491d7f425
EugeneTime <eugenetime@bk.ru>
2018-10-27 22:46:30 +0300

    feature: - implement final function

--- sum-of-other.js
+++ sum-of-other.js
@@ -1,6 +1,13 @@
 function sumOfOther (sample) {
     let sumItems = 0;
     let result = [];
-    
+    let ItemLength = sample.length;
+
+    for (let i = 0; i <= ItemLength - 1; i++) {
+        let arrWithoutItem = sample.filter(item => item !== sample[i]);
+        sumItems = arrWithoutItem.reduce((acc, item) => acc + item);
+        result.push(sumItems);
+    }
+
     return result;
 }
